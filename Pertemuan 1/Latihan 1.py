551cf8514bc581fc80e9436637ab00d8674fbac9
Refayaseptianno16 <120187120+Refayaseptianno16@users.noreply.github.com>
2022-12-12 12:12:13 +0700

    Add files via upload
    push

---
+++ Pertemuan 1/Latihan 1.py
@@ -0,0 +1,3 @@
+print("hello dunia!!!!!!!")
+print("apa kabar dunia?")
+print("instalasi berhasil")
---
+++ Pertemuan 1/Latihan 3.py
@@ -0,0 +1,24 @@
+# Variabel adalah tempat menyimpan data 
+
+# menaru / assigment nilai
+a = 10
+X = 5
+panjang = 1000
+
+# pemanggilan pertama 
+print("Nilai a = ",a)
+print("Nilai x = ",X)
+print("Nilai panjang = ",panjang)
+
+# penamaan
+nilai_y = 15 # dengan mengunakan under score
+juta10 = 10000000 # ini boleh 
+nilaiZ = 17.5 # ini boleh
+
+# pemnaggilan kedua 
+print("Nilai a = ",a)
+a = 7
+print("Nilai a = ", a)
+# asigment indirect 
+b = a
+print("Nilai b= ",b)
